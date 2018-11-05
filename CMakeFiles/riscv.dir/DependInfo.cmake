44f10d99bfcc15a3a93dff45ff69864131dbb315
chaitanyaPaikara <chaitanya1445@gmail.com>
2018-11-05 21:21:29 +0530

    Non-overlapped Pipeline

---
+++ CMakeFiles/riscv.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/chaitanya/RISC-V/Sample/part1.c" "/home/chaitanya/RISC-V/Sample/CMakeFiles/riscv.dir/part1.c.o"
+  "/home/chaitanya/RISC-V/Sample/part2.c" "/home/chaitanya/RISC-V/Sample/CMakeFiles/riscv.dir/part2.c.o"
+  "/home/chaitanya/RISC-V/Sample/riscv.c" "/home/chaitanya/RISC-V/Sample/CMakeFiles/riscv.dir/riscv.c.o"
+  "/home/chaitanya/RISC-V/Sample/utils.c" "/home/chaitanya/RISC-V/Sample/CMakeFiles/riscv.dir/utils.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/riscv.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/riscv.dir/riscv.c.o"
+  "CMakeFiles/riscv.dir/part1.c.o"
+  "CMakeFiles/riscv.dir/part2.c.o"
+  "CMakeFiles/riscv.dir/utils.c.o"
+  "riscv.pdb"
+  "riscv"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/riscv.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
